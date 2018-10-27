a881cca5d02c70b6d23b6337a6814643f9b65f80
abhinay reddy <abhinayreddy520@gmail.com>
2018-10-27 17:51:32 +0530

    cleared every errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ const Header=()=>(
             </h1>     
 <p> <NavLink to="/"activeClassName="is-active" exact={true}>ExpenseDashboard</NavLink></p>
 <p>  <NavLink to="/create" activeClassName="is-active">AddExpenseDashboard</NavLink></p>
-<p><NavLink to="/edit" activeClassName="is-active">EditExpensePage</NavLink></p>
 <p> <NavLink to="/help" activeClassName="is-active">HelpPage</NavLink></p>
 
     </header>
