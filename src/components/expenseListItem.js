321150fe909a309a31c1888e643b3e911dd821ec
abhinay reddy <abhinayreddy520@gmail.com>
2018-10-28 10:09:55 +0530

    setup currency inr

--- src/components/expenseListItem.js
+++ src/components/expenseListItem.js
@@ -6,7 +6,7 @@ const ExpenseListItem = ({id,description,amount,createdAt})=>{
     return (
         <div>
    <h3>description: <Link to={`/edit/${id}`}> {description} </Link> </h3>
-            <p>amount:{numeral(amount/100).format('$0,0.00')}</p>
+            <p>amount:'&#8377;'{numeral(amount/100).format('$0,0.00')}</p>
             <p>createdAt:{moment(createdAt).format('Do MMMM, YYYY')}</p>
           
         </div>
