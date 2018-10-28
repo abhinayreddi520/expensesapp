756f94e31353422740423314446ef1558f853efb
abhinay reddy <abhinayreddy520@gmail.com>
2018-10-28 09:28:19 +0530

    setup currency and time

--- src/components/expenseListItem.js
+++ src/components/expenseListItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 const ExpenseListItem = ({id,description,amount,createdAt})=>{
     return (
         <div>
    <h3>description: <Link to={`/edit/${id}`}> {description} </Link> </h3>
-            <p>amount:{amount}</p>
-            <p>createdAt:{createdAt}</p>
+            <p>amount:{numeral(amount/100).format('$0,0.00')}</p>
+            <p>createdAt:{moment(createdAt).format('Do MMMM, YYYY')}</p>
           
         </div>
     );
 }; 
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
